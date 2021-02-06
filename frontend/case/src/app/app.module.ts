6f2692f640c3c0218d08a444483b393f99890d28
Toby Lausch <toby@lauscht.com>
2021-02-06 15:34:29 +0100

    Added mattooltip and add lesson.

--- frontend/case/src/app/app.module.ts
+++ frontend/case/src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WeeklyComponent } from './weekly/weekly.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LessongComponent } from './lessong/lessong.component';
 import { LessonComponent } from './lesson/lesson.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeeklyComponent,
-    LessongComponent,
     LessonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatTooltipModule,
     BrowserAnimationsModule,
     NgbModule
   ],
--- frontend/case/src/app/weekly/weekly.component.ts
+++ frontend/case/src/app/weekly/weekly.component.ts
@@ -40,4 +40,8 @@ export class WeeklyComponent implements OnInit {
 
   }
 
+  add() {
+
+  }
+
 }
