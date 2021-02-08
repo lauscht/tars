53881734d6b6315c3401f866a409f00172fb9a3a
thegrayhulk <thegrayhulk@googlemail.com>
2021-02-08 22:27:58 +0100

    Add flyout.

---
+++ frontend/case/src/app/shared/flyout/flyout.component.ts
@@ -0,0 +1,41 @@
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,  
+} from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flyout',
+  templateUrl: './flyout.component.html',
+  styleUrls: ['./flyout.component.css'],
+  animations: [
+    trigger('openClose', [     
+      state('open', style({
+        top: '70vh',             
+      })),
+      state('closed', style({
+        top: '100vh',        
+      })),
+      transition('open => closed', [
+        animate('0.3s')
+      ]),
+      transition('closed => open', [
+        animate('0.3s')
+      ]),
+    ]),
+  ],
+})
+export class FlyoutComponent implements OnInit {
+  
+  @Input()
+  visible: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
