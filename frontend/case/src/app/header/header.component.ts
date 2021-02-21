a27e1ed05a79ef9c1ab135300f19d1b1cec7b427
Toby Lausch <toby@lauscht.com>
2021-02-21 21:59:43 +0100

    Moved todo to app header.

--- frontend/case/src/app/header/header.component.ts
+++ frontend/case/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'case-header',
   templateUrl: './header.component.html',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public todos;
+
+  constructor() {
+    this.todos = [
+      {
+        component: "weekly.component",
+        todos: [
+          'Manchmal gibt es auch samstags kurse. Ansicht wechselbar 5,6 oder 7 Tage.',
+          'getBackground() shall we switch to props get background() instead?',
+          'We should provide changeable colors and gradient settings :-)',
+          'Add color property to courses.',
+          'Change alpha with day time',
+          'white color and low alpha have to small contrast.'
+        ],
+      }, {
+        component: "course.component",
+        todos: [
+          'Add grades to pupils.'
+        ]
+      },
+    ];
+   }
 
   ngOnInit(): void {
   }
