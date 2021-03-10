7b1ff60fa97cd0d62e6e5ee8b1f98cd2e0c5c1e2
derHulk <thegrayhulk@googlemail.com>
2021-03-10 21:42:05 +0100

    Add functions to save and cancel.

--- frontend/case/src/app/weekly/weekly.component.ts
+++ frontend/case/src/app/weekly/weekly.component.ts
@@ -7,7 +7,7 @@ import { FlyInOutAnimation } from '../fly-in.animation';
 import { ColorService } from '../shared/color.service';
 import { Course } from '../course/course.entity';
 import { DoCheck } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'weekly',
@@ -20,9 +20,11 @@ export class WeeklyComponent implements OnInit, DoCheck {
   public days: DateTime[];
   public selected: Lesson;
   public previous: Lesson[];
-  public future:Lesson[];
-  public editContent:boolean;
-  public editHomework:boolean;
+  public future: Lesson[];
+  public selectedContent: string;
+  public selectedHomework: string;
+  public editContent: boolean;
+  public editHomework: boolean;
   public day;
   public end;
 
@@ -30,21 +32,21 @@ export class WeeklyComponent implements OnInit, DoCheck {
   editLessonState = 'out';
   constructor(
     private lessonService: LessonService,
-    private colorService: ColorService,    
+    private colorService: ColorService,
   ) {
     this.day = DateTime.local();
-   }
+  }
 
-   getLessonsOfTheDay(day){
-     const weekday = day.weekday;
-     return this.lessonService.getLessons().filter((l) =>
-     l.start.weekday === weekday);
-   }
+  getLessonsOfTheDay(day) {
+    const weekday = day.weekday;
+    return this.lessonService.getLessons().filter((l) =>
+      l.start.weekday === weekday);
+  }
 
-   getColorOfTheDay(day){
+  getColorOfTheDay(day) {
     const weekday = day.weekday;
     return this.colorService.WeekColorMap.get(weekday);
-   }
+  }
 
   ngOnInit(): void {
     this.updateDays();
@@ -52,24 +54,24 @@ export class WeeklyComponent implements OnInit, DoCheck {
 
   ngDoCheck(): void {
 
-    if(!this.selected && this.days && this.days.length > 0){
+    if (!this.selected && this.days && this.days.length > 0) {
       const lessons = this.getLessonsOfTheDay(this.days[0]);
 
-      if(lessons.length > 0){
+      if (lessons.length > 0) {
         this.show(lessons[0]);
       }
 
     }
-    
+
   }
 
   updateDays() {
     const weekly = new WeeklyHelper();
     const start = weekly.startOfWeek(this.day);
-    this.end = start.plus({days:weekly.days});
+    this.end = start.plus({ days: weekly.days });
     const days = [];
-    for(var c = 0; c < weekly.days; c++){
-      days.push(start.plus({days: c}));
+    for (var c = 0; c < weekly.days; c++) {
+      days.push(start.plus({ days: c }));
     }
     this.days = days;
   }
@@ -79,25 +81,31 @@ export class WeeklyComponent implements OnInit, DoCheck {
   }
   show(lesson: Lesson) {
 
-    if(lesson != this.selected){
+    if (lesson != this.selected) {
       this.editContent = (lesson.content === null);
       this.editHomework = (lesson.homework == null);
-    }    
-    
-    this.selected = lesson;    
+      this.selectedContent = lesson.content;
+      this.selectedHomework = lesson.homework;
+    }
+
+    this.selected = lesson;
     this.future = this.lessonService.getFuture(lesson);
     this.previous = this.lessonService.getPrevious(lesson);
     this.editLessonState = 'in';
-    
+
   }
 
-  save(){
+  save() {
+    this.selected.content = this.selectedContent;
+    this.selected.homework = this.selectedHomework;
     //Looks like MatSnackBar is incompatible! Todo check why!
     //this.snackBar.open('Changes saved.');
   }
 
-  cancel(){
+  cancel() {
     //this.snackBar.open('Changes canceled.');
+    this.selectedContent = this.selected.content;
+    this.selectedHomework = this.selected.homework;
   }
 
 }
