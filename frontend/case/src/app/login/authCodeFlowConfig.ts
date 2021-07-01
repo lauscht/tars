5a1e447d25ebcf50cde1430cb52aa214a45139ef
derHulk <thegrayhulk@googlemail.com>
2021-07-01 20:05:20 +0200

    Working sample but without code flow. Because Google does not support it for spa's.

--- frontend/case/src/app/login/authCodeFlowConfig.ts
+++ frontend/case/src/app/login/authCodeFlowConfig.ts
@@ -2,22 +2,22 @@ import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const AuthCodeFlowConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: '',
+  issuer: 'https://accounts.google.com',
 
   // URL of the SPA to redirect the user to after login
-  redirectUri: window.location.origin + '/index.html',
+  redirectUri: window.location.origin + '/login',
 
   // The SPA's id. The SPA is registerd with this id at the auth-server
   // clientId: 'server.code',
-  clientId: '',
+  clientId: '....apps.googleusercontent.com',
 
   // Just needed if your auth server demands a secret. In general, this
   // is a sign that the auth server is not configured with SPAs in mind
   // and it might not enforce further best practices vital for security
   // such applications.
-  // dummyClientSecret: 'secret',
+  //dummyClientSecret: 'secret',
 
-  responseType: 'code',
+  //responseType: 'token',
 
   // set the scope for the permissions the client should request
   // The first four are defined by OIDC.
@@ -25,5 +25,6 @@ export const AuthCodeFlowConfig: AuthConfig = {
   // The api scope is a usecase specific one
   scope: 'openid profile email',
 
+  strictDiscoveryDocumentValidation: false,
   showDebugInformation: true,
 };
--- frontend/case/src/app/login/login.component.ts
+++ frontend/case/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { OAuthService } from 'angular-oauth2-oidc';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
 import { AuthCodeFlowConfig } from './authCodeFlowConfig';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -9,17 +10,60 @@ import { AuthCodeFlowConfig } from './authCodeFlowConfig';
 })
 export class LoginComponent implements OnInit {
 
+  userName: string = null;
+  flightCount: number;
+
+  get accessToken() {
+    return this.oauthService.getAccessToken();
+  }
+
+  get refreshToken() {
+    return this.oauthService.getRefreshToken();
+  }
+
+  get idToken() {
+    return this.oauthService.getIdToken();
+  }
+
+  get userClaims(){
+    return this.oauthService.getIdentityClaims();
+  }
+
   constructor(private oauthService: OAuthService) {
-    oauthService.configure(AuthCodeFlowConfig);    
-    oauthService.setupAutomaticSilentRefresh();
+    
+    //importent we can not use responseType: 'code',
+    //remarks:
+    //https://medium.com/@ishmeetsingh/google-auth-integration-with-angular-5-and-angular-oauth2-oidc-ed01b997e1df
+    //https://github.com/damienbod/angular-auth-oidc-client/issues/399
+    //https://stackoverflow.com/questions/60724690/using-google-oidc-with-code-flow-and-pkce    
+
+    oauthService.configure(AuthCodeFlowConfig);        
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+
+    // oauthService.setupAutomaticSilentRefresh();    
+    // oauthService.loadDiscoveryDocumentAndTryLogin().then(_ => {
+    //   const claims = this.oauthService.getIdentityClaims();
+    //   if (!claims) {
+    //     return null;
+    //   }    
+    // });
+
+    oauthService.events
+      .pipe(filter(e => e.type === 'token_received'))
+      .subscribe(_ => {
+        console.debug('state', this.oauthService.state);
+        this.oauthService.loadUserProfile();
+      });
+
   }
 
   ngOnInit(): void {
   }
 
   login(): void {
-    console.log('Login pressed..')
-    this.oauthService.initLoginFlow();
+    console.log('Login pressed..')    
+    this.oauthService.initLoginFlow();    
   }
 
   logout(): void {
