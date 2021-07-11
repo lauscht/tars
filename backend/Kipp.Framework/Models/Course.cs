cf621347fe95306dd265113bf0c8a21da176ee4d
Toby Lausch <toby@lauscht.com>
2021-07-11 12:49:52 +0200

    Typed lesson entity.

---
+++ backend/Kipp.Framework/Models/Course.cs
@@ -0,0 +1,6 @@
+namespace Kipp.Framework.Models{
+    public class Course{
+        public string Name {get; set;}
+        public string Subject {get; set;}
+    }
+}
--- backend/Kipp.Framework/Models/Lesson.cs
+++ backend/Kipp.Framework/Models/Lesson.cs
@@ -1,5 +1,16 @@
 namespace Kipp.Framework.Models{
     public class Lesson{
-        
+        /// <summary>
+        /// Room where the lesson is taking place.
+        /// </summary>
+        public string Room {get; set;}
+        /// <summary>
+        /// Planned content of the lesson.
+        /// </summary>
+        public string Content {get; set;}
+        /// <summary>
+        /// Homeworks given in this lesson.
+        /// </summary>
+        public string Homework {get; set;}
     }
 }
